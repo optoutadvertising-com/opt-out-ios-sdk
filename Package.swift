2d3cb796dbda3b3f036f2e356100058dabb95b01
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2025-09-16 10:21:43 +0200

    Update to 2.0.1

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 
 struct PackageMetadata {
-	static let version = "1.1.5"
-	static let checksum = "76a5b91a5da979163540c4411be70050913f3e80ff72c599dd3d7e8d32502387"
+	static let version = "2.0.1"
+	static let checksum = "2e30e3fc12676ce3c303a7fd330cd868cdde9c567755789ac8f830abea29d411"
 }
 
 let package = Package(
@@ -19,10 +19,20 @@ let package = Package(
 			targets: ["OptOutAdvertisingSDK"]
 		)
 	],
+	dependencies: [
+		.package(url: "https://github.com/optoutadvertising-com/om-ios-sdk.git", from: "1.5.5")
+	],
 	targets: [
-		.binaryTarget(
+		.target(
 			name: "OptOutAdvertisingSDK",
-			url: "https://github.com/optoutadvertising-com/ios-sdk/releases/download/\(PackageMetadata.version)/OptOutAdvertisingSDK.xcframework.zip",
+			dependencies: [
+				.target(name: "OptOutAdvertisingSDKBinary"),
+				.product(name: "OMSDK_Pinchnl", package: "om-ios-sdk")
+			]
+		),
+		.binaryTarget(
+			name: "OptOutAdvertisingSDKBinary",
+			url: "https://github.com/optoutadvertising-com/opt-out-ios-sdk/releases/download/\(PackageMetadata.version)/OptOutAdvertisingSDK.xcframework.zip",
 			checksum: PackageMetadata.checksum
 		)
 	]
---
+++ Sources/OptOutAdvertisingSDK/OptOut.swift
@@ -0,0 +1,1 @@
+// This file is required to satisfy SPM validation.
